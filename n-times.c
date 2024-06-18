74f533cfa63578d425154d8d718b37a31a0a73de
Hasib Mohammad Mahede Hasan <hasib9963@gmail.com>
2024-06-18 21:50:11 +0600

    practice day 2

---
+++ n-times.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+   int i,N;
+    scanf("%d",&N);
+    for(i=1;i<=N;i=i+1)
+    {
+        printf("I Love Practice\n");
+    }
+    return 0;
+}
---
+++ sum-2-numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int A,B,sum;
+    scanf("%d %d",&A,&B);
+    sum = A+B;
+    printf("%d",sum);
+}
